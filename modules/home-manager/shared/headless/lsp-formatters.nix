0ec334d8a79f87f07057993931116747e44deb21
Richard Gill <richardgill@users.noreply.github.com>
2025-10-26 13:17:08 +0000

    nix-private sha: 8148d4b (#24)

--- modules/home-manager/shared/headless/lsp-formatters.nix
+++ modules/home-manager/shared/headless/lsp-formatters.nix
@@ -2,11 +2,25 @@
   pkgs,
   ...
 }:
-# See also: modules/home-manager/dot-files/mise/config.toml.mustache
 {
   home.packages = with pkgs; [
-    # LSP servers (not available in mise)
+    # LSP servers
+    astro-language-server
+    biome
+    gopls
     kotlin-language-server
+    lua-language-server
     nixd
+    nodePackages.vscode-langservers-extracted
+    nodePackages."@tailwindcss/language-server"
+    pyright
+    ruff
+    vtsls
+
+    # Formatters
+    gofumpt
+    nodePackages.prettier
+    prettierd
+    stylua
   ];
 }
--- modules/system/nixos/graphical/optional/virt-manager.nix
+++ modules/system/nixos/graphical/optional/virt-manager.nix
@@ -37,6 +37,19 @@
 
   programs.virt-manager.enable = true;
 
+  systemd.services.libvirt-default-network = {
+    description = "Start libvirt default network";
+    after = ["libvirtd.service"];
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "${pkgs.libvirt}/bin/virsh net-start default";
+      ExecStop = "${pkgs.libvirt}/bin/virsh net-destroy default";
+      User = "root";
+    };
+  };
+
   users.users.${vars.userName}.extraGroups = [ "libvirtd" ];
 
   environment.systemPackages = with pkgs; [
