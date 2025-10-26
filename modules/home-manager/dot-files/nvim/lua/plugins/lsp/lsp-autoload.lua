c6d9245d4f8f76c79857ff6a44cc93365f2d33d8
Richard Gill <richard@rgill.co.uk>
2025-10-26 17:06:08 +0000

    nix-private sha: 0da6913

--- modules/home-manager/dot-files/nvim/lua/plugins/lsp/lsp-autoload.lua
+++ modules/home-manager/dot-files/nvim/lua/plugins/lsp/lsp-autoload.lua
@@ -1,16 +1,21 @@
 local lsp_path = vim.fn.stdpath 'config' .. '/after/lsp'
 
--- Finds all lsps in after/lsp/*.lua and enables the lsp.
--- Grab default configs: https://github.com/neovim/nvim-lspconfig/tree/master/lsp and copy them into after/lsp/<new-lsp>.lua
 -- LSP and Formatters installed in lsp-formatters.nix
--- Health check with:  :checkhealth vim.lsp
--- LSP logs for current buffer:  :lua vim.cmd.edit(vim.lsp.log.get_filename())
+-- Installing a new lsp:
+--   Grab a default config https://github.com/neovim/nvim-lspconfig/tree/master/lsp
+--   Copy it into after/lsp/<new-lsp>.lua
+
+-- Finds all lsps in after/lsp/*.lua and enables each lsp.
 for name, type in vim.fs.dir(lsp_path) do
   if type == 'file' and name:match '%.lua$' then
     local server = name:match '(.+)%.lua$'
     vim.lsp.enable(server)
   end
 end
+
+-- Debugging:
+--   Health check with:  :checkhealth vim.lsp
+--   LSP logs for current buffer:  :lua vim.cmd.edit(vim.lsp.log.get_filename())
 
 -- keep Lazy happy
 return {}
