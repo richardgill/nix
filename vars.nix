e6df7447bb79bdb84d4d9eda071e71297a0a523d
Richard Gill <richard@rgill.co.uk>
2026-01-08 18:16:07 +0000

    nix-private sha: 33a5e76

--- vars.nix
+++ vars.nix
@@ -1,6 +1,6 @@
 let
   sshPublicKeyPersonal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIASgK9NpwI8oA8cYyWdvwjrz2dTN1C5uSSqOjOnkxzEr";
-  sshPublicKeyPhone = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILznqOJBKWuIYDCIn2pePrUkzHHGydA1R5QlGmYtIai8 u0_a559@localhost";
+  sshPublicKeyPhone = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL+3hHH8ymCh7n6r6O5oibwrrOC21fCzeS/te5Rzv+zy u0_a564@localhost";
   sshPublicKeyWork = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMYgMHFX6iZGRl+UGb0lS53/JhORzSc68MB1n6/1NlGF rich@macbookair-work";
   sshPublicKeyUm790 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKSxne+q64ORA+I1cdRqL2oTrrcJqY4Ev2PxzU1h+STt rich@um790";
 in
